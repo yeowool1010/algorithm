c61e9101c2affd97cf71a7e96fa3c856a503e173
yeowool1010 <qyoong3579@gmail.com>
2022-07-09 01:19:50 +0900

    더하기 사이클 미완

--- 백준/알고리즘 스터디_1/반복문2_더하기 사이클.js
+++ 백준/알고리즘 스터디_1/반복문2_더하기 사이클.js
@@ -1,21 +1,35 @@
-let A = Number(prompt("첫 번째 수 입력"));
-let B = Number(prompt("두 번째 수 입력"));
+let input = prompt("숫자입력");
 
 function solution() {
-    let result = "";
-    if(A > B) {
-        result = ">"
-    } else if(A < B) {
-        result = "<"
-    } else if(A === B) {
-        result = "=="
+    let num = input;
+    let sum;
+    let i = 0;
+    while(true) {
+        i++
+
+        sum = Math.floor(num / 10) + num % 10;
+        num = (num % 10) * 10 + sum % 10;
+        // sum = Number(num[0]) + Number(num[1]);
+        // if(sum.length === 1) {
+        //     num = Number(num[1]) + Number(sum[0]);
+        // } 
+        // if(sum.length === 2) {
+        //     num = Number(num[1]) + Number(sum[1]);
+        // }
+        if(input === num) {
+            break;
+        }
     }
-    alert(result);
+    alert(i);
 }
 
 solution();
 
 
-// A가 B보다 큰 경우에는 '>'를 출력한다.
-// A가 B보다 작은 경우에는 '<'를 출력한다.
-// A와 B가 같은 경우에는 '=='를 출력한다.
+// 26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 
+// 새로운 수는 84이다. 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 
+// 새로운 수는 26이다.
+
+// 위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
+
+// N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
