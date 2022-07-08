ba4acb5dccb938298d8fe19ace5cc21e7847c176
yeowool1010 <qyoong3579@gmail.com>
2022-07-09 03:00:45 +0900

    백준 답안 정리

--- 백준/알고리즘 스터디_1/1차원배열2_평균은 넘겠지.js
+++ 백준/알고리즘 스터디_1/1차원배열2_평균은 넘겠지.js
@@ -1,21 +1,14 @@
-let A = Number(prompt("첫 번째 수 입력"));
-let B = Number(prompt("두 번째 수 입력"));
+let input = prompt("입력값");
+let arr = input.split('/');
+console.log(arr)
 
 function solution() {
     let result = "";
-    if(A > B) {
-        result = ">"
-    } else if(A < B) {
-        result = "<"
-    } else if(A === B) {
-        result = "=="
-    }
-    alert(result);
+    
+    // alert(result);
 }
 
 solution();
 
 
-// A가 B보다 큰 경우에는 '>'를 출력한다.
-// A가 B보다 작은 경우에는 '<'를 출력한다.
-// A와 B가 같은 경우에는 '=='를 출력한다.
+// 5/5 50 50 70 80 100/7 100 95 90 80 70 60 50/3 70 90 80/3 70 90 81/9 100 99 98 97 96 95 94 93 91
